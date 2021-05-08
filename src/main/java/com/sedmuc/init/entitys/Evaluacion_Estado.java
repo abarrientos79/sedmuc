4593c72273a90f4deda46fa44939ee5c8e29410f
abarr <abarr@192.168.0.7>
2021-05-08 06:25:00 -0300

    version 1.3 funcionando sin detalles 2

---
+++ src/main/java/com/sedmuc/init/entitys/Evaluacion_Estado.java
@@ -0,0 +1,81 @@
+package com.sedmuc.init.entitys;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+
+@Entity
+public class Evaluacion_Estado implements Serializable {
+
+	private static final long serialVersionUID = -7129022574976089696L;
+
+
+		@Id
+		@GeneratedValue(strategy=GenerationType.AUTO, generator="native")
+		@GenericGenerator(name="native",strategy="native")
+		private Long id;
+		
+		@Column
+		private String descripcion;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getDescripcion() {
+			return descripcion;
+		}
+
+		public void setDescripcion(String descripcion) {
+			this.descripcion = descripcion;
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((descripcion == null) ? 0 : descripcion.hashCode());
+			result = prime * result + ((id == null) ? 0 : id.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Evaluacion_Estado other = (Evaluacion_Estado) obj;
+			if (descripcion == null) {
+				if (other.descripcion != null)
+					return false;
+			} else if (!descripcion.equals(other.descripcion))
+				return false;
+			if (id == null) {
+				if (other.id != null)
+					return false;
+			} else if (!id.equals(other.id))
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return "Evaluacion_Estado [id=" + id + ", descripcion=" + descripcion + "]";
+		}
+		
+	
+}
